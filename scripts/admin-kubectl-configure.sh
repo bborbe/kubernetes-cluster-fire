a97b8fd3150e5d4057d7cc8290df1116befa189f
Benjamin Borbe <bborbe@rocketnews.de>
2017-01-16 20:59:59 +0100

    update k8s

--- scripts/admin-kubectl-configure.sh
+++ scripts/admin-kubectl-configure.sh
@@ -7,10 +7,10 @@ set -o errtrace
 
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})
 
-mkdir -p $HOME/.kube/hm
-kubectl config set-cluster hm-cluster --server=https://172.16.22.10:443 --certificate-authority=$HOME/.kube/hm/ca.pem
-kubectl config set-credentials hm-admin --certificate-authority=$HOME/.kube/hm/ca.pem --client-key=$HOME/.kube/hm/admin-key.pem --client-certificate=$HOME/.kube/hm/admin.pem
-kubectl config set-context hm-system --cluster=hm-cluster --user=hm-admin
-kubectl config use-context hm-system
+mkdir -p $HOME/.kube/fire
+kubectl config set-cluster fire-cluster --server=https://172.16.22.10:443 --certificate-authority=$HOME/.kube/fire/ca.pem
+kubectl config set-credentials fire-admin --certificate-authority=$HOME/.kube/fire/ca.pem --client-key=$HOME/.kube/fire/admin-key.pem --client-certificate=$HOME/.kube/fire/admin.pem
+kubectl config set-context fire-system --cluster=fire-cluster --user=fire-admin
+kubectl config use-context fire-system
 
 echo "test with 'kubectl get nodes'"
